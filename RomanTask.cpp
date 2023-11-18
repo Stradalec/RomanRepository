e82406881a5c80ae3d438b37118609d4d378bbed
Stradalec <pochtatipo6@gmail.com>
2023-11-18 09:19:19 +0700

    Added new feature

--- RomanTask.cpp
+++ RomanTask.cpp
@@ -3,7 +3,7 @@
 * Лабораторная №8                 *
 * Дата: 12.11.2023                *
 * Название: Арабские числа        *
-* https://onlinegdb.com/joQoh_PSz *
+* https://onlinegdb.com/492RUIOCn *
 **********************************/
 
 #include <iostream>
@@ -14,18 +14,29 @@ using namespace std;
 
 
 int main() {
+  int arrayLength;
   int number;
   string romanNumber;
+  string inputRomanNumber;
+  int outputNumber = 0;
+  char checkArray[] = "IVXLCDM";
+  char firstTemp;
+  char secondTemp;
+  char thirdTemp;
+  char fourthTemp;
   
   cout << "Arabic number: ";
   cin >> number;
+  cout << "Roman number: ";
+  cin >> inputRomanNumber;
+  arrayLength = inputRomanNumber.length();
+  
 
   while (number > 0) {
     if (number / 1000 > 0) {
       int temp = 1000;
       romanNumber += "M";
       number = number - temp;
-      cout << number;
       if (number  >= 900) {
         romanNumber += "CM";
         number = number - 900;
@@ -104,7 +115,77 @@ int main() {
     
   }
   
-  cout << "Roman number: " << romanNumber;
+  for (int checkIndex = 0; checkIndex < arrayLength; ++checkIndex) {
+    firstTemp = inputRomanNumber[checkIndex];
+    secondTemp = inputRomanNumber[checkIndex + 1];
+    if (firstTemp == checkArray[6] ) {
+      outputNumber += 1000;
+    } else if (firstTemp == checkArray[5] ) {
+      outputNumber += 500;
+    } else if (firstTemp == checkArray[4] ) {
+      if (secondTemp == checkArray[6] ) {
+        outputNumber += 900;  
+      } else if (secondTemp == checkArray[5] ) {
+        outputNumber += 400;  
+      } else {
+        outputNumber += 100;  
+      } 
+        
+    } else if (firstTemp == checkArray[3] ) {
+      outputNumber += 50;
+    } else if (firstTemp == checkArray[2] ) {
+      if (secondTemp == checkArray[3] ) {
+        outputNumber += 40;  
+      } else if (secondTemp == checkArray[4] ) {
+        outputNumber += 90;  
+      } else {
+        outputNumber += 10;
+      }
+        
+      } else if (firstTemp == checkArray[1] ) {
+        if (secondTemp == checkArray[0] ) {
+          if (thirdTemp == checkArray[0] ) {
+            if (fourthTemp == checkArray[0] ) {
+              outputNumber += 8;  
+            }
+            outputNumber += 7;
+          }
+          outputNumber += 6;
+        } else {
+          outputNumber += 5;  
+        }
+        
+      } else if (firstTemp == checkArray[0] ) {
+        if (secondTemp == checkArray[0] ) {
+          if (thirdTemp == checkArray[0] ) {
+            outputNumber += 3;
+          }
+          outputNumber += 2;
+          
+        } else if (secondTemp == checkArray[1] ) {
+          outputNumber += 4;  
+        } else if (secondTemp == checkArray[2] ) {
+          outputNumber += 9;
+        } else {
+        outputNumber += 1;  
+        }
+      
+      } else {
+        cout << "Wrong symbol" << endl;  
+      }
+     
+    }  
+
+  if (outputNumber == 0) {
+    cout << "Can not create new arabic number" << endl;  
+  } else {
+    cout << "New arabic number: " << outputNumber << endl;
+  }
+  
+  cout << "New roman number: " << romanNumber;
   
   return 0;
 }
+
+
+
